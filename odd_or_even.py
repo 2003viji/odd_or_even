3978277b9cdc6c37f4a8d889dc4d5bce0505c9f4
2003viji <85789102+2003viji@users.noreply.github.com>
2022-10-20 17:21:23 +0530

    Add files via upload

---
+++ odd_or_even.py
@@ -0,0 +1,14 @@
+#  This code is to find  odd or Even
+
+#  The input is taken from the User and then it is valuated
+a=int(input("Enter your num :"))
+if a%2==0:
+    print("even")
+else:    
+    print("odd")
+
+
+
+#   The another single way to find the given number is odd or even
+
+print("EVEN" if(int(input("Enter your value :")))%2==0 else "ODD")
